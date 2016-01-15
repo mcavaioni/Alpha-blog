9113f81c18727c8f5a3a8a8df38a6d39d6f2d6cd
mikecavs <michele.cavaioni@gmail.com>
2016-01-15 15:08:41 +0000

    update action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,11 @@ class ArticlesController < ApplicationController
         @article=Article.new
     end
     
+    def edit
+        @article=Article.find(params[:id])
+    end
+    
+  
     def create
         # render plain: params[:article].inspect
         @article = Article.new(article_params)
@@ -12,6 +17,16 @@ class ArticlesController < ApplicationController
             redirect_to article_path(@article)
         else
             render 'new'
+        end
+    end
+    
+    def update
+        @article=Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice]="article successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
         end
     end
     
