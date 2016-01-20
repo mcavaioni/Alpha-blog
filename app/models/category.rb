185e8c10f921a2bf5418353aaa4b6776c2378f00
mikecavs <michele.cavaioni@gmail.com>
2016-01-20 15:38:37 +0000

    delete category feature

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
     has_many :article_categories
-    has_many :articles, through: :article_categories
+    has_many :articles, through: :article_categories, dependent: :destroy
     
     validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25}
     # validates_uniqueness_of :name
